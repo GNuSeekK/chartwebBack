9c1652aab28b3d00a0d5089f086321ee0783f342
Keesung <person813@gmail.com>
2023-10-07 15:45:53 +0900

    initial commit

---
+++ src/test/java/stock/chart/ChartApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
