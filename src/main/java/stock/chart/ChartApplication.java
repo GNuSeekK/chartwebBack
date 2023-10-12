832f8038c8ffc1eeb606cff4830ca14d40fda2d0
Keesung <person813@gmail.com>
2023-10-12 10:33:59 +0900

    develop Entity revised - auditing add

--- src/main/java/stock/chart/ChartApplication.java
+++ src/main/java/stock/chart/ChartApplication.java
@@ -1,8 +1,14 @@
 package stock.chart;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChartApplication {
 
