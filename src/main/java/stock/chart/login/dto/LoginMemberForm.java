a6cc64867b0bf6ab84eb77ed6b7ebc761b3ac67e
Keesung <person813@gmail.com>
2023-10-13 01:46:27 +0900

    LoginMemberForm validity add

--- src/main/java/stock/chart/login/dto/LoginMemberForm.java
+++ src/main/java/stock/chart/login/dto/LoginMemberForm.java
@@ -1,11 +1,14 @@
 package stock.chart.login.dto;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginMemberForm {
 
+    @Email(message = "이메일 형식에 맞게 입력해주세요.")
     @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
     @NotBlank(message = "비밀번호를 입력해주세요.")
