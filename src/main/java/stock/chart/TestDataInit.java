4d507ef8d030a16ecd051bcd49c8a7b3bd4155c9
Keesung <person813@gmail.com>
2023-10-12 10:34:47 +0900

    develop Entity revised - test data init add

---
+++ src/main/java/stock/chart/TestDataInit.java
@@ -0,0 +1,31 @@
+package stock.chart;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import stock.chart.domain.Member;
+import stock.chart.member.repository.MemberRepository;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TestDataInit {
+
+    private final MemberRepository memberRepository;
+    /**
+     * 확인용 초기 데이터 추가
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+
+        Member member = new Member("ssafy@ssafy.com", "ssafy", "싸피");
+        Member member2 = new Member("member@ssafy.com", "member", "멤버");
+
+        memberRepository.save(member);
+        memberRepository.save(member2);
+
+    }
+}
